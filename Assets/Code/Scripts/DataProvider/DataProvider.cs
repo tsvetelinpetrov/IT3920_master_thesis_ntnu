6d4d3d02a211219c1bb0a3bf193eb8593186014b
Tsvetelin Petrov <42267885+tsvetelinpetrov@users.noreply.github.com>
2024-11-01 23:44:48 +0100

    Add some testing UI

--- Assets/Code/Scripts/DataProvider/DataProvider.cs
+++ Assets/Code/Scripts/DataProvider/DataProvider.cs
@@ -1,25 +1,46 @@
-using System.Collections.Generic;
+using Newtonsoft.Json;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class DataProvider : MonoBehaviour
 {
 
+    // Text object to display the data
+    public TMP_Text DebugText;
+
     void Start()
     {
-        IDataSource dataSource = DataSourceFactory.GetDataSource();
-        // dataSource.GetControlsByDays(50,
-        //     (controls) =>
-        //     {
-        //         Debug.Log(controls.Count);
-        //     }
-        // );
 
+    }
+
+    // Get plant model from data source
+    public void GetPlantModel()
+    {
+        IDataSource dataSource = DataSourceFactory.GetDataSource();
         dataSource.GetPlantObjModel(
             (model) =>
             {
-                PlantRenderer plantRenderer = new PlantRenderer();
-                plantRenderer.renderPlant(model);
+                // Put first 1000 characters of the model into the debug text
+                DebugText.text = model.Substring(0, 1000);
+                //PlantRenderer plantRenderer = new PlantRenderer();
+                //plantRenderer.renderPlant(model);
+            }
+        );
+    }
+
+    // Get controls by days from data source
+    public void GetControlsByDays()
+    {
+        IDataSource dataSource = DataSourceFactory.GetDataSource();
+        dataSource.GetControlsByDays(30,
+            (controls) =>
+            {
+                // Get controls as json string
+                string jsonString = JsonConvert.SerializeObject(controls, Formatting.Indented);
+
+                Debug.Log(jsonString);
+                DebugText.text = jsonString;
             }
         );
     }
