9cca5b84fb2a157bb49a2b289809ffced9bc725e
ELBETTALO <eb.oussama3@gmail.com>
2024-11-06 16:22:35 +0100

    Api work
    Added get for other endpoints

---
+++ Assets/Code/Scripts/DataProvider/Entity/Disruptive.cs
@@ -0,0 +1,12 @@
+using System;
+
+[System.Serializable]
+public class Disruptive
+{
+    public long MeasurementTime { get; set; }
+    public string SensorID { get; set; }
+    public float Temperature { get; set; }
+    public float Humidity { get; set; }
+
+}
+
